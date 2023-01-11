461d3a913ac1021d089374cdad51ad7c41cb7f8a
Giocatory <nivolyas@gmail.com>
2023-01-11 17:54:12 +0300

    10.01.23

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Order
+
 
 # Create your views here.
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', {
+        'object_list': object_list
+    })
